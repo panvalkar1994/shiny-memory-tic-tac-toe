0165904ed528bba45daa0bdd0366a43b51c5a64c
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-10-17 19:14:07 +0530

    game complete or selected move is marked

--- src/index.js
+++ src/index.js
@@ -44,6 +44,9 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+      return
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     // const xIsNext = !this.state.xIsNext;
     this.setState({
