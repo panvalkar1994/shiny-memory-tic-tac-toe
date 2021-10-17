c7ffcc3327895f984f562fcc26bd27157a7ff51a
vaibhav panvalkar <vpanvalkar@outlook.com>
2021-10-17 18:57:05 +0530

    next player

--- src/index.js
+++ src/index.js
@@ -18,13 +18,18 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares:Array(9).fill(null),
+      xIsNext:true,
     }
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares})
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    // const xIsNext = !this.state.xIsNext;
+    this.setState({
+      squares:squares,
+      xIsNext:!this.state.xIsNext,
+    })
   }
 
   renderSquare(i) {
@@ -37,7 +42,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? "X" : "O");
 
     return (
       <div>
